24d72e622b026d70b604a1b780fe93ed6519af79
Njokinjoroge <zeejaymorgan@gmail.com>
2025-11-20 17:31:11 +0300

    create businnes app views

--- myproject/business/views.py
+++ myproject/business/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+class BusinessObjectList(APIView):
+    """
+    Return a list of fictional business objects.
+    """
+    def get(self, request):
+        # Fake data
+        data = [
+            {"id": 1, "name": "Object A"},
+            {"id": 2, "name": "Object B"},
+            {"id": 3, "name": "Object C"},
+        ]
+        # TODO: check permissions in real access control
+        return Response(data)
--- myproject/permissions_app/models.py
+++ myproject/permissions_app/models.py
@@ -1,19 +1,20 @@
-
 from django.db import models
 from users.models import User
 
 class Role(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
-class BusinessElement(models.Model):
-    name = models.CharField(max_length=100)
+class Resource(models.Model):
+    name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
 
 class AccessRoleRule(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    element = models.ForeignKey(BusinessElement, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     can_read = models.BooleanField(default=False)
     can_write = models.BooleanField(default=False)
