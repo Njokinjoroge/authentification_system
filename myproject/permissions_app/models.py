dce458c1cd1425be2fbfbbb0e7c322447c962140
Njokinjoroge <zeejaymorgan@gmail.com>
2025-11-20 20:30:28 +0300

    create models in permissions

--- myproject/permissions_app/models.py
+++ myproject/permissions_app/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+from django.db import models
 from users.models import User
 
 class Role(models.Model):
+    name = models.CharField(max_length=30)
+
+class BusinessElement(models.Model):
     name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-class Resource(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
 
 class AccessRoleRule(models.Model):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
-    can_read = models.BooleanField(default=False)
-    can_write = models.BooleanField(default=False)
+    element = models.ForeignKey(BusinessElement, on_delete=models.CASCADE)
+    read_permission = models.BooleanField(default=False)
+    read_all_permission = models.BooleanField(default=False)
+    create_permission = models.BooleanField(default=False)
+    update_permission = models.BooleanField(default=False)
+    update_all_permission = models.BooleanField(default=False)
+    delete_permission = models.BooleanField(default=False)
+    delete_all_permission = models.BooleanField(default=False)
+
