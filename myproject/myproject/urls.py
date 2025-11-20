e8228ab6e0d8d57febb61781536ad8f990ff8bc0
Njokinjoroge <zeejaymorgan@gmail.com>
2025-11-20 17:21:09 +0300

    create user urls

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import RegisterView, LoginView, LogoutView, UserUpdateView, UserDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('update/', UserUpdateView.as_view()),
+    path('delete/', UserDeleteView.as_view()),
 ]
