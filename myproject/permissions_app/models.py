54b4fd41363def7aa470eaf6e864c7421a51edd8
Njokinjoroge <zeejaymorgan@gmail.com>
2025-11-20 17:22:18 +0300

    create permission app models

--- myproject/permissions_app/models.py
+++ myproject/permissions_app/models.py
@@ -1,3 +1,19 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from users.models import User
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class BusinessElement(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class AccessRoleRule(models.Model):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    element = models.ForeignKey(BusinessElement, on_delete=models.CASCADE)
+    can_read = models.BooleanField(default=False)
+    can_write = models.BooleanField(default=False)
